2b8133f299eb8ff09633b960146c7598cd93cd89
Thomas Ryan E E <107304775+THOMAS0034@users.noreply.github.com>
2024-03-20 22:00:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const input=document.getElementById("input-box");
+const list = document.getElementById("item");
+
+function create(){
+  if(input.value==''){
+    alert("please write something");
+  }
+  else
+  {
+    let li =document.createElement("li");
+    li.innerHTML=input.value;
+    list.appendChild(li);
+    let span =document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+  }
+  input.value="";
+  save();
+}
+list.addEventListener("click",function(e){
+  if(e.target.tagName==="LI"){
+    e.target.classList.toggle("checked");
+    save();
+  }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    save();
+  }
+},false);
+function save(){
+  localStorage.setItem("data",item.innerHTML)
+}
+function show(){
+ list.innerHTML=localStorage.getItem("data"); 
+}
+show();
